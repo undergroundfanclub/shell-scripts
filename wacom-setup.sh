0296aa54f72f6b9ec438bfe45634c1ae656b5e56
matth <matth@ugfc-bebop>
2021-04-26 21:39:03 -0400

    new file:   subscriptions.json 	new file:   wacom-setup.sh 	new file:   youtubefeed-rss

---
+++ wacom-setup.sh
@@ -0,0 +1,6 @@
+#1/bin/sh
+
+echo 'setting up wacom device 14-16 to xrandr HEAD-0 device'
+xsetwacom set 9 MapToOutput HEAD-0
+xsetwacom set 10 MapToOutput HEAD-0
+xsetwacom set 15 MapToOutput HEAD-0
